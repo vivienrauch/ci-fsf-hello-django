09abbd7d1b50f3d3787b47dfa9eb105d635b0fa1
vivienrauch <vivienmirjam@gmail.com>
2023-05-12 13:25:31 +0000

    Add test to forms

--- todo/test_views.py
+++ todo/test_views.py
@@ -3,8 +3,16 @@ from django.test import TestCase
 # Create your tests here.
 
 
-class TestDjango(TestCase):
+class TestViews(TestCase):
 
-    def test_this_thing_works(self):
-        self.assertEqual(1, 1)
+    def test_get_todo_list(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/todo_list.html')
+
+
+    
+
+    
+       
 
