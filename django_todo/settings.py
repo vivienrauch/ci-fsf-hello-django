ec00ffb305765e1547402879cdf4cd9920b74e40
vivienrauch <vivienmirjam@gmail.com>
2023-05-13 09:40:51 +0000

    Prepare to deploy to Heroku

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -1,3 +1,9 @@
+from pathlib import Path
+import os
+import dj_database_url
+import env
+
+
 """
 Django settings for django_todo project.
 
@@ -74,12 +80,12 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#   'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+# }
 
 
 # Password validation
